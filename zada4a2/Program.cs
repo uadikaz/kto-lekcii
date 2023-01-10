d1bfe7c4b0e1f9556c39400c3aa6c4558bc49dd6
Vladislav S <uadik67@gmail.com>
2023-01-10 18:06:14 +0300

    zada4ki

---
+++ zada4a2/Program.cs
@@ -0,0 +1,14 @@
+﻿// zada4a2
+
+using System;  
+namespace New_Project  
+{ public static class Program   
+{ public static void Main()   
+{ long a, b, c, i, max; 
+  Console.Write("a b c: ");  
+  var line = Console.ReadLine().Split(" ");  
+  a = long.Parse(line[0]); 
+  b = long.Parse(line[1]); 
+  c = long.Parse(line[2]); 
+  Console.WriteLine("Max = " + 
+  Math.Max(a, Math.Max(b, c))); } } }
