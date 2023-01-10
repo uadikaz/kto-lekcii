777f89d7d0afbc8496427f032ca399acb8afa072
Vladislav S <uadik67@gmail.com>
2023-01-10 18:51:13 +0300

    added zada4a1

--- semminar/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿//zada4a 2
-
-a=int(input("Первое число:")) 
-mx=a b=int(input("Второе число:")) 
-if b>mx:mx=b c=int(input("Третье число:")) 
-if c>mx:mx=c print('Наибольшее:',mx)
---
+++ zada4a1/Program.cs
@@ -0,0 +1,7 @@
+﻿//zada4a 2
+
+a=int(input("Первое число:")) ;
+mx=a b=int(input("Второе число:")) ;
+if b>mx:mx=b c=int(input("Третье число:")) ;
+//находим наибольшее
+if c>mx:mx=c print('Наибольшее:',mx);
